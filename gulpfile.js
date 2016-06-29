6e21e6f72500a252336ef81243de6dc4f65fba95
Max Kaplan <maxkaplan@Sourcetoads-MacBook-Pro-3.local>
2016-06-29 16:36:20 -0400

    fixing folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require("gulp");
 const babel = require("gulp-babel");
 
 gulp.task("babelize", () => {
-  return gulp.src("./src/**/*.js")
+  return gulp.src("./src/**/**/start/*.js")
   .pipe(babel({presets:['es2015']}))
   .pipe(gulp.dest("./dist"));
 });
 
 gulp.task("babel", () => {
-  return gulp.watch("./src/**/*.js", ['babelize']);
+  return gulp.watch("./src/**/**/start/*.js", ['babelize']);
 });
 
 gulp.task("default",["babelize","babel"]);
