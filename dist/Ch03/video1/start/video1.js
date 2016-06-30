2ceb8a5eade684de7894eab9e8042f4d05c1ef2e
Max Kaplan <kaplanmaxe@users-MacBook-Pro.local>
2016-06-30 00:03:27 -0400

    adding more to promises

--- dist/Ch03/video1/start/video1.js
+++ dist/Ch03/video1/start/video1.js
@@ -8,3 +8,31 @@ var db = new _mockDb.Database(); //ES6 Import syntax. We will go over this later
 db.find(function (data) {
   console.log(data);
 });
+
+function firstPromise(status) {
+  var promise = new Promise(function (fulfill, reject) {
+    if (status === true) {
+      fulfill("The promise is fulfilled.");
+    } else {
+      reject(new Error("The promise is rejected"));
+    }
+  });
+  return promise;
+}
+
+// function secondPromise() {
+//   let promise = new Promise(function(fulfill,reject) {
+//     fulfill({status:true});
+//   });
+//   return promise;
+// }
+
+firstPromise(true).then(function (status) {
+  console.log(status);
+  // return secondPromise();
+}, function (error) {
+  console.log(error.message);
+});
+// .then(function(status) {
+//   console.log(status);
+// });
--- src/Ch03/video1/start/video1.js
+++ src/Ch03/video1/start/video1.js
@@ -5,3 +5,32 @@ var db = new Database();
 db.find(function(data) {
   console.log(data);
 });
+
+function firstPromise(status) {
+  let promise = new Promise(function(fulfill,reject) {
+    if (status===true) {
+      fulfill("The promise is fulfilled.");
+    }
+    else {
+      reject(new Error("The promise is rejected"));
+    }
+  });
+  return promise;
+}
+
+// function secondPromise() {
+//   let promise = new Promise(function(fulfill,reject) {
+//     fulfill({status:true});
+//   });
+//   return promise;
+// }
+
+firstPromise(true).then(function(status) {
+  console.log(status);
+  // return secondPromise();
+}, function(error) {
+  console.log(error.message);
+})
+// .then(function(status) {
+//   console.log(status);
+// });
