5372806f3de6bb6105812bbb1a02b709c94313d8
Max Kaplan <mekapl03@syr.edu>
2016-07-04 14:36:46 -0400

    updating finished video1 for Ch01

--- src/Ch02/video1/finish/video1.js
+++ src/Ch02/video1/finish/video1.js
@@ -1,26 +1,19 @@
 //Let and const
-
-const firstName = "Max";
-// firstName = "Kaplan"; WILL THROW ERROR BECAUSE IT IS ALREADY SET AND NOW "READ-ONLY"
-// let firstName = "Max"; WILL THROW ERROR BECAUSE CONST IS ALREADY DECLARED
-console.log(firstName);
+const name = "Max";
 
 let lastName = "Kaplan";
-lastName = "Smith"; //WILL WORK BECAUSE WITH LET WE CAN OVERWRITE ORIGINAL VALUE
-// let lastName = "Smith"; WILL NOT WORK BECAUSE ALREADY DECLARED
+lastName = "Smith";
+
 console.log(lastName);
 
 function testing() {
   let foo = 1;
   const bar = 3;
-  const animal = "Dog";
   if (true) {
     let foo = 2;
     const bar = 4;
-    const color = "Red";
-    console.log("INSIDE: " + foo + " " + bar + " " + animal); //Outputs: INSIDE 2 4 Dog
+    console.log("INSIDE: " + foo + " " + bar);
   }
-  console.log("OUTSIDE: " + foo + " " + bar); //Outputs: OUTSIDE 1 3
-  //console.log(color); Color is undefined
+  console.log("OUTSIDE: " + foo + " " + bar);
 }
 testing();
