b7b58e47977d2eece89a10b732f1fff1f9f9cac2
Max Kaplan <mekapl03@syr.edu>
2016-07-04 15:42:29 -0400

    fixing up ch01 video5 finish

--- src/Ch02/video5/finish/video5.js
+++ src/Ch02/video5/finish/video5.js
@@ -2,18 +2,11 @@
 
 const names = ['max','john','michael','sarah'];
 
-//ES5 Way
-let capitalNames = names.map(function(name) {
-  return name.toUpperCase();
-});
-console.log(capitalNames);
-
-//ES6 Basic Arrow Function Syntax
-let num = () => 3 * 2;
+const num = () => 3*2;
 console.log(num());
 
-//How we can use it in our map function
-let capitalNames6 = names.map((name) => {
+const capitalNames = names.map(name => {
   return name.toUpperCase();
 });
-console.log(capitalNames6);
+
+console.log(capitalNames);
