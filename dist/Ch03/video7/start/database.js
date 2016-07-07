43e227f637141745629ed6a852de1b2e45141b65
Max Kaplan <mekapl03@syr.edu>
2016-07-07 15:22:23 -0400

    fixing start ch03 video7

--- dist/Ch03/video7/start/database.js
+++ dist/Ch03/video7/start/database.js
@@ -1,15 +1,1 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var _class = function _class() {
-  _classCallCheck(this, _class);
-
-  console.log("This class has been instatiated!");
-};
-
-exports.default = _class;
--- src/Ch03/video7/start/animal.js
+++ src/Ch03/video7/start/animal.js
@@ -1,11 +1,1 @@
-export const animalName = "Tiger";
 
-export function jump(animal) {
-  return `The ${animal} jumped!`;
-}
-
-export class Animal {
-  roar() {
-    return "ROARRRRRR!";
-  }
-}
